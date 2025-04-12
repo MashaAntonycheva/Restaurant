d5419fff3f5fda16c841e8c70935203c162d3e2a
MashaAntonycheva <mashakomardina0@gmail.com>
2025-04-12 11:46:34 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,9 @@
+import VideoPlayerCollection from './VideoPlayer.js'
+import Reviews from './Reviews.js'
+
+new VideoPlayerCollection()
+new Reviews('review'); 
+
+document.getElementById('dataToday').valueAsDate = new Date();
+
+
